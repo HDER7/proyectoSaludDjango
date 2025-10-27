6edcb4cf4cfc75d69b228404ed6e48b15170bc88
Sebasbtmz <beltranandres1000@gmail.com>
2025-10-26 20:56:38 -0500

    Se agrega el readme y se agregan más información para el enfermedades i ciie10

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# 🏥 Proyecto — Historia Clínica Electrónica
+
+Este sistema permite gestionar la historia clínica electrónica de pacientes conforme a la Resolución 866 de 2021 del Ministerio de Salud de Colombia.
+
+## 🚀 Requisitos
+
+- Python 3.10+
+- Django 4.x
+- MySQL Server
+- Conexión configurada en `settings.py` con `ENGINE = 'django.db.backends.mysql'`
+
+## ⚙️ Instalación
+
+1. Clona el repositorio:
+   ```bash
+   git clone https://tu-repo.git
+   cd salud_y_vida
+
+2. Instala las dependencias:
+    pip install -r requirements.txt
+
+3. configura la base de datos en settings.py:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'salud_db',
+            'USER': 'tu_usuario',
+            'PASSWORD': 'tu_contraseña',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
+
+4. Ejecuta las migraciones en el Bash
+    python manage.py makemigrations
+    python manage.py migrate
+
+5. Ejecuta, esto nos carga los datos base del modelo catalogo, y los que tenemos    en el apps.py
+
+    python manage.py runserver
+
+6. Datos del admin por defecto para ingresar:
+    username='admin',
+    email='admin@gmail.com',
+    password='admin123'
--- gestion_salud/apps.py
+++ gestion_salud/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 from django.db.utils import OperationalError
 from django.core.exceptions import ObjectDoesNotExist
-
 from django.contrib.auth import get_user_model
 
 class GestionSaludConfig(AppConfig):
@@ -66,6 +65,12 @@ class GestionSaludConfig(AppConfig):
                 Discapacidad.objects.bulk_create([
                     Discapacidad(cod_categoria_discapacidad='01', nombre_discapacidad='Física'),
                     Discapacidad(cod_categoria_discapacidad='02', nombre_discapacidad='Auditiva'),
+                    Discapacidad(cod_categoria_discapacidad='03', nombre_discapacidad='Visual'),
+                    Discapacidad(cod_categoria_discapacidad='04', nombre_discapacidad='Sordoceguera'),
+                    Discapacidad(cod_categoria_discapacidad='05', nombre_discapacidad='Intelectual'),
+                    Discapacidad(cod_categoria_discapacidad='06', nombre_discapacidad='Psicosocial (mental)'),
+                    Discapacidad(cod_categoria_discapacidad='07', nombre_discapacidad='Múltiple'),
+                    Discapacidad(cod_categoria_discapacidad='08', nombre_discapacidad='Ninguna'),
                 ])
 
             # Modalidades
@@ -94,13 +99,52 @@ class GestionSaludConfig(AppConfig):
                 CatalogoEnfermedadesHuerfanas.objects.bulk_create([
                     CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Marfan', tipo='A1'),
                     CatalogoEnfermedadesHuerfanas(nombre='Fibrosis quística', tipo='B2'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Ehlers-Danlos', tipo='A3'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Esclerosis lateral amiotrófica', tipo='A4'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Rett', tipo='A5'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Ataxia de Friedreich', tipo='A6'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Prader-Willi', tipo='A7'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Angelman', tipo='A8'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Turner', tipo='A9'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Noonan', tipo='B1'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Williams', tipo='B3'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Alport', tipo='B4'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Sjögren', tipo='B5'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Cushing', tipo='B6'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Klinefelter', tipo='B7'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de X frágil', tipo='B8'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Leigh', tipo='B9'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Dravet', tipo='C1'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Moebius', tipo='C2'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Joubert', tipo='C3'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Alström', tipo='C4'),
+                    CatalogoEnfermedadesHuerfanas(nombre='Síndrome de Bardet-Biedl', tipo='C5'),
                 ])
-
             # Diagnósticos CIE-10
             if CIE10.objects.count() == 0:
                 CIE10.objects.bulk_create([
                     CIE10(nombre='Diabetes mellitus tipo 2', tipo='E11'),
                     CIE10(nombre='Hipertensión esencial', tipo='I10'),
+                    CIE10(nombre='Fiebre tifoidea', tipo='A01'),
+                    CIE10(nombre='Enteritis debida a Salmonella', tipo='A02'),
+                    CIE10(nombre='Shigelosis debida a Shigella dysenteriae', tipo='A030'),
+                    CIE10(nombre='Shigelosis debida a Shigella flexneri', tipo='A031'),
+                    CIE10(nombre='Shigelosis debida a Shigella boydii', tipo='A032'),
+                    CIE10(nombre='Shigelosis debida a Shigella sonnei', tipo='A033'),
+                    CIE10(nombre='Infección debida a Escherichia coli enteropatógena', tipo='A040'),
+                    CIE10(nombre='Tuberculosis pulmonar, confirmada bacteriológicamente', tipo='A150'),
+                    CIE10(nombre='Tuberculosis pulmonar, no confirmada bacteriológicamente', tipo='A151'),
+                    CIE10(nombre='Tuberculosis de otros órganos respiratorios', tipo='A152'),
+                    CIE10(nombre='Tuberculosis miliar', tipo='A159'),
+                    CIE10(nombre='Varicela sin complicaciones', tipo='B01'),
+                    CIE10(nombre='Sarampión sin complicaciones', tipo='B05'),
+                    CIE10(nombre='Rubeola sin complicaciones', tipo='B06'),
+                    CIE10(nombre='Hepatitis viral aguda tipo A', tipo='B15'),
+                    CIE10(nombre='Hepatitis viral aguda tipo B', tipo='B16'),
+                    CIE10(nombre='Hepatitis viral aguda tipo C', tipo='B17'),
+                    CIE10(nombre='Hepatitis viral crónica tipo B', tipo='B18'),
+                    CIE10(nombre='Hepatitis viral crónica tipo C', tipo='B182'),
+                    CIE10(nombre='VIH con infección sintomática', tipo='B20'),
                 ])
 
             # ======================================================
@@ -112,7 +156,7 @@ class GestionSaludConfig(AppConfig):
             if not User.objects.filter(username='admin').exists():
                 User.objects.create_superuser(
                     username='admin',
-                    email='admin@saludyvida.local',
+                    email='admin@gmail.com',
                     password='admin123'
                 )
 
